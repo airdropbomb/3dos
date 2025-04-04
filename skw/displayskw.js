bceea070d652170e9ac9ef340aadebe678bfbd05
airdropbomb <saginaw135@gmail.com>
2025-04-04 17:23:37 +0630

    Update displayskw.js

--- skw/displayskw.js
+++ skw/displayskw.js
@@ -1,25 +1,9 @@
-const chalk = require('chalk');
-
-const welcomeskw = chalk.hex('#00CED1')(`
+console.log(`
  █████╗ ██████╗ ██████╗     ███╗   ██╗ ██████╗ ██████╗ ███████╗
 ██╔══██╗██╔══██╗██╔══██╗    ████╗  ██║██╔═══██╗██╔══██╗██╔════╝
 ███████║██║  ██║██████╔╝    ██╔██╗ ██║██║   ██║██║  ██║█████╗  
 ██╔══██║██║  ██║██╔══██╗    ██║╚██╗██║██║   ██║██║  ██║██╔══╝  
 ██║  ██║██████╔╝██████╔╝    ██║ ╚████║╚██████╔╝██████╔╝███████╗
 ╚═╝  ╚═╝╚═════╝ ╚═════╝     ╚═╝  ╚═══╝ ╚═════╝ ╚═════╝ ╚══════╝
+by btctrader
 `);
-
-function displayskw() {
-  console.log(welcomeskw);
-  console.log(chalk.hex('#00CED1')(" ╔══════════════════════════════════════════════════════════════╗"));
-  console.log(chalk.hex('#00CED1')(" ║ ≣  3DOS Autobot by ADB Node                                  ║"));
-  console.log(chalk.hex('#00CED1')(" ║══════════════════════════════════════════════════════════════║"));
-  console.log(chalk.hex('#00CED1')(" ║ ➤   Auto claim Daily                                        ║"));
-  console.log(chalk.hex('#00CED1')(" ║ ➤   Auto Ping                                               ║"));
-  console.log(chalk.hex('#00CED1')(" ║ ➤   Multiple                                                ║"));
-  console.log(chalk.hex('#00CED1')(" ║ ➤   Definitely Eligible                                     ║"));
-  console.log(chalk.hex('#00CED1')(" ╚══════════════════════════════════════════════════════════════╝"));
-  console.log(chalk.hex('#00CED1')("   ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░"));
-}
-
-module.exports = { displayskw };
