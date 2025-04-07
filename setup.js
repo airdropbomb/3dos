84845f7a68212528e504862d36e50c5cd2f53b29
Hunga9k50doker <19520101@gm.uit.edu.vn>
2025-04-08 04:36:36 +0700

    ud

--- setup.js
+++ setup.js
@@ -1,8 +1,13 @@
 const { jwtDecode } = require("jwt-decode");
-const { loadData, saveJson } = require("./utils");
+const { loadData, saveJson, saveData } = require("./utils");
 
 const main = async () => {
   const tokens = loadData("tokens.txt");
+  const accounts = loadData("data.txt");
+  const emails = accounts.map((acc) => {
+    const [email, pass] = acc.split("|");
+    return email;
+  });
   if (tokens.length == 0) {
     console.error("Tokens not found in tokens.txt");
     return;
@@ -12,6 +17,9 @@ const main = async () => {
     for (const token of tokens) {
       const decodedToken = jwtDecode(token);
       const { email } = decodedToken;
+      if (!emails.includes(email)) {
+        accounts.push(`${email}|pass`);
+      }
       await saveJson(
         email,
         JSON.stringify({
@@ -19,6 +27,7 @@ const main = async () => {
         }),
         "localStorage.json"
       );
+      await saveData(accounts, "data.txt");
     }
   } catch (error) {
     console.error("Error decoding JWT:", error);
